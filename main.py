bd710e1eacfcd4b8dec99987f93994390fff53a7
Vladimir <saakyan2001@gmail.com>
2022-08-14 20:34:53 +0200

    kursach

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
-from pprint import pprint
 import requests
+from yadisk import Yadisk
+import urllib
 
 
 class VK:
     def __init__(self, ttoken, user_id, version='5.131'):
-       self.token = ttoken
-       self.id = user_id
-       self.version = version
-       self.params = {'access_token': self.token, 'v': self.version}
+        self.token = ttoken
+        self.id = user_id
+        self.version = version
+        self.params = {'access_token': self.token, 'v': self.version}
 
     def users_info(self):
-       url = 'https://api.vk.com/method/users.get'
-       params = {'user_ids': self.id}
-       response = requests.get(url, params={**self.params, **params})
-       return response.json()
+        url = 'https://api.vk.com/method/users.get'
+        params = {'user_ids': self.id}
+        response = requests.get(url, params={**self.params, **params})
+        return response.json()
 
     def get_photos(self):
-       url = 'https://api.vk.com/method/photos.get'
-       params = {
-           'access_token': ttoken,
-           'v': 5.131,
-           'album_id': 'profile',
-           'extended': 1
-       }
-       resp = requests.get(url, params=params).json()
-       for alb in resp['response']['items']:
-           for img in alb['sizes']:
-               if img['type'] == 'w':
-                   print(img['url'])
+        url = 'https://api.vk.com/method/photos.get'
+        params = {
+            'access_token': ttoken,
+            'v': 5.131,
+            'album_id': 'profile',
+            'extended': 1
+        }
+        resp = requests.get(url, params=params).json()
+        for idx, alb in enumerate(resp['response']['items']):
+            for img in alb['sizes']:
+                if img['type'] == 'w':
+                    urllib.request.urlretrieve(img['url'], f'image.jpg')
+                    ya.upload_file_to_disk(f'test/image_{idx}.jpg', f'image.jpg')
 
 
 if __name__ == '__main__':
+    ya = Yadisk(token="AQAAAABZ1l9vAADLW2o_p71eAERDs7EdDJ6e3iI")
     ttoken = 'vk1.a.aNqIqjDwTwy-2o0QQeSVrgWbCUzvLQjqsyZynaLBorSSaVUG84JE-riCqiVR-r_BsDz8uUBTnwNF8VQnMCssTDdprhNqakzOqAkPE2fku-sW2YECTStco-YxD-j008-1cwW7snKJzpiBR8Q8yK3sIJkYBA9KXbbhtvO8xf21Cx99fuwwHCXQjzjnFybq86dX'
     user_id = '741761650'
     vk = VK(ttoken, user_id)
     vk.get_photos()
-
-
--- yadisk.py
+++ yadisk.py
@@ -3,11 +3,16 @@ from pprint import pprint
 
 
 class Yadisk:
+    def __init__(self, token):
+        self.token = token
+
+
     def get_headers(self):
         return {
             'Content-Type': 'application/json',
             'Authorization': 'OAuth {}'.format(self.token)
         }
+
 
     def _get_upload_link(self, disk_file_path):
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
@@ -16,6 +21,7 @@ class Yadisk:
         response = requests.get(upload_url, headers=headers, params=params)
         pprint(response.json())
         return response.json()
+
 
     def upload_file_to_disk(self, disk_file_path, filename):
         href_json = self._get_upload_link(disk_file_path=disk_file_path)
@@ -26,7 +32,3 @@ class Yadisk:
             print('Success')
 
 
-if __name__ == '__main__':
-    token_ya = ''
-    ya = YandexDisk(token_ya)
-    ya.upload_file_to_disk('', '')
